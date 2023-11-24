3b5b4bf3fa40909105ff368f0c0a5cd314a81e00
LucasGabrielAntonete <lucasantonete@gmail.com>
2023-11-24 15:05:10 -0300

    feat do SimpleJWT

--- config/urls.py
+++ config/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import include, path
-
 from rest_framework.routers import DefaultRouter
-
 from app.views import TranslationViewSet
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 router = DefaultRouter()
 
@@ -12,4 +14,6 @@ router.register(r"translations", TranslationViewSet)
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include(router.urls)),
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
