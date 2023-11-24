9af5275a85b8879c8341542765f76255dff10986
RafaelBochi <rafaelbochibarros@gmail.com>
2023-11-24 14:54:28 -0300

    Update add usuario no translation #1

--- app/models/translation.py
+++ app/models/translation.py
@@ -3,7 +3,9 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.utils import timezone
 
+from usuario.models import Usuario
 class Translation(models.Model): 
+    user = models.ForeignKey(Usuario, on_delete=models.CASCADE)
     question = models.CharField(max_length=200)
     answer = models.CharField(max_length=200)
     date = models.DateTimeField('date published', default=timezone.now, blank=True, null=True)
