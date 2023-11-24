cd7ba272d4e72c42ac319b05256960cbec99c2e2
LucasGabrielAntonete <lucasantonete@gmail.com>
2023-11-24 17:50:14 -0300

    fix #17 - termino da Autenticação

--- config/settings.py
+++ config/settings.py
@@ -108,7 +108,12 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 REST_FRAMEWORK = {
- 
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
+    ],
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.DjangoModelPermissions",
+    ],
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
