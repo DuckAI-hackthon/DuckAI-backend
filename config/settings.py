f246c17994da7b4b55e7d4ad810bf35ae1546534
LucasGabrielAntonete <lucasantonete@gmail.com>
2023-11-24 18:51:29 -0300

    fix no usuario e inserção do swagger documents api

--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "django_extensions",
+    "drf_spectacular",
     "rest_framework",
     "rest_framework_simplejwt",
     "usuario",
@@ -117,6 +118,7 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
+     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
  }
 
 
@@ -144,3 +146,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = "usuario.Usuario"
 
+SPECTACULAR_SETTINGS = {
+    "TITLE": "Duck IA",
+    "DESCRIPTION": "API para gerenciamento do Duck IA",
+    "VERSION": "1.0.0",
+}
--- config/urls.py
+++ config/urls.py
@@ -1,15 +1,18 @@
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-
+from usuario.router import router as usuario_router
 from app.views import AiViewSet, FavoriteViewSet, FunctionViewSet, HistoricViewSet
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
-
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView,
+)
 router = DefaultRouter()
-
 router.register(r"ais", AiViewSet)
 router.register(r"favorites", FavoriteViewSet)
 router.register(r"functions", FunctionViewSet)
@@ -18,10 +21,22 @@ router.register(r"historics", HistoricViewSet)
 from src.search import search
 
 urlpatterns = [
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/swagger/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
+    path(
+        "api/redoc/",
+        SpectacularRedocView.as_view(url_name="schema"),
+        name="redoc",
+    ),
+    path("api/", include(usuario_router.urls)),
     path("admin/", admin.site.urls),
-    path("", include(router.urls)),
+    path("api/", include(router.urls)),
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
-
     path("ai/", search, name="ai"),
+    
 ]
